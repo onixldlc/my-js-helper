2209d38ce207f8a3e46fd4aa38cf53e1b1041417
onixldlc <81515667+onixldlc@users.noreply.github.com>
2022-02-17 17:18:48 +0700

    Create payloadSender.js

---
+++ stuff/payloadSender.js
@@ -0,0 +1,25 @@
+var payload = (clientVersion, continuation) => `
+{
+	"context": {
+		"client": {
+			"clientName": "WEB",
+			"clientVersion": "${clientVersion}"
+		}
+	},
+	"continuation": "${continuation}"
+}`
+
+
+async function grabBatch(url, version, continuation){
+	var retValue = ""
+	await fetch(url, {
+		"headers": {
+			"Accept": "*/*",
+			"Content-Type": "application/json"
+		},
+		"body": `${payload(version,continuation)}`,
+		"method": "POST"
+	})
+	.then(async (responseData) => {retValue = await responseData.json()});
+	return retValue;
+}
