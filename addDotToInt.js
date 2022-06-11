9edd03454756c8dea16bedb549ff2b9dc96452e0
onixldlc <81515667+onixldlc@users.noreply.github.com>
2022-06-11 23:03:12 +0800

    added other function and change var name

--- addDotToInt.js
+++ addDotToInt.js
@@ -1,6 +1,11 @@
 function addDotToInt(num){
 	num = parseInt(num)
 	if(num<1000){return String(num)}
-	let currency = addDotToInt(num/1000)+".000"
-	return currency;
+	let dotted = addDotToInt(num/1000)+".000"
+	return String(dotted);
+}
+
+function convertToIDR(num){
+	let currency = parseInt(String(num).replaceAll(".",""))
+	return `Rp${addDotToInt(currency)},00`
 }
