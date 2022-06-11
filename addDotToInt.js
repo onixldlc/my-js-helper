e948250d368c7545d0bd522da7cb7209d07239e9
onixldlc <81515667+onixldlc@users.noreply.github.com>
2022-06-11 22:36:52 +0800

    Update addDotToInt.js

--- addDotToInt.js
+++ addDotToInt.js
@@ -1,6 +1,6 @@
-function convertIntToCurrency(num){
+function addDotToInt(num){
 	num = parseInt(num)
 	if(num<1000){return String(num)}
-	let currency = convertIntToCurrency(num/1000)+".000"
+	let currency = addDotToInt(num/1000)+".000"
 	return currency;
 }
