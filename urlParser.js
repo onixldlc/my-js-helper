b231c14d72ba5e9bd4b2116c8f9dee4a90856861
onixldlc <81515667+onixldlc@users.noreply.github.com>
2022-07-21 17:45:31 +0800

    Create urlParser.js

---
+++ urlParser.js
@@ -0,0 +1,20 @@
+function parseURLParams(url) {
+  var queryStart = url.indexOf("?") + 1,
+    queryEnd   = url.indexOf("#") + 1 || url.length + 1,
+    query = url.slice(queryStart, queryEnd - 1),
+    pairs = query.replace(/\+/g, " ").split("&"),
+    parms = {}, i, n, v, nv;
+
+  if (query === url || query === "") return;
+
+  for (i = 0; i < pairs.length; i++) {
+    nv = pairs[i].split("=", 2);
+    n = decodeURIComponent(nv[0]);
+    v = decodeURIComponent(nv[1]);
+
+    if (!parms.hasOwnProperty(n)){
+      parms[n]=v;
+    }
+  }
+  return parms;
+}
