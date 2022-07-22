246d67da07122d64c6b1d4bc7d2e3b35fb4d986f
onixldlc <81515667+onixldlc@users.noreply.github.com>
2022-07-22 11:25:05 +0800

    Update youtubeVideoIDregex.js

--- youtubeVideoIDregex.js
+++ youtubeVideoIDregex.js
@@ -1,4 +1,6 @@
-function getYtIdFromUrl(Url){
-	var pattern = /(?:https:\/\/)(?:www\.youtube\.com\/|youtu\.be)(?:watch\?v=|embed\/|\/)(.*)/
-	return Url.match(pattern)[1]
+function getYtInfoFrmUrl(ytUrl){
+	var pattern = /(?:https:\/\/)(?:www\.youtube\.com\/|youtu\.be)(?:watch\?v=|embed\/|\/)(\w+)(?:(?:&t=)(\d+)|)/
+	var matched = ytUrl.match(pattern)
+	matched.shift()
+	return matched
 }
