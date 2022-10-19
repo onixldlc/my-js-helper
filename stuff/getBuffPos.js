c12bd0dda378cfb085e8f800c630fa11f8332751
onixldlc <81515667+onixldlc@users.noreply.github.com>
2022-10-19 20:57:04 +0700

    Create getBuffPos.js

---
+++ stuff/getBuffPos.js
@@ -0,0 +1,37 @@
+
+function closestBracketForward(buffer, index){
+  for(let i=index, i < buffer.length, i++){
+    if(buffer[i]=="{"){
+      return i
+    }
+  }
+  throw "Error: closest '{' was not found in buffer" 
+}
+
+
+
+function getBuffPos(buffer, index){
+  let counter = 0
+  let len = buffer.length
+  let start = closestBracketForward(buffer, index)
+  let end = 0
+  let i = start
+
+  do{
+    
+    let character = buffer[i];
+    if(character == "{"){
+      counter++
+    }else if(character == "}"){
+      counter--
+    }
+    i++
+    if( i >= len ){ 
+      throw "Error: either you forgot to add '}' or the buffer is corrupted" 
+    }
+
+  }while( counter && i < len)
+  end=i
+
+  return [start, end]
+}
