7c3d61d52021115fb1eb6f383d5958848e7cd47f
onixldlc <81515667+onixldlc@users.noreply.github.com>
2022-10-20 17:13:33 +0700

    Update grabValueFromJsonStr.js

--- stuff/grabValueFromJsonStr.js
+++ stuff/grabValueFromJsonStr.js
@@ -1,48 +1,62 @@
+
 function grabValueFrom(buffer, key){
-  let keyPos = buffer.indexOf(key)
-  if (keyPos == -1){console.log("key not found !!!");return}
-  let buffPos = getBuffPos(buffer, keyPos)
-  let data = buffer.slice(...buffPos)
-  return JSON.parse(data)
+	let keyPos = buffer.indexOf(key)
+	let buffPos = getBuffPos(buffer, keyPos)
+	let data = buffer.slice(...buffPos)
+	return data
 }
 
-function closestBracketForward(buffer, index){
-  for(let i=index; i < buffer.length; i++){
-    if(buffer[i]=="{" ||  buffer[i]=="["){
-      return i
-    }
-  }
-  throw "Error: closest '{' or '[' was not found in buffer" 
+function closestValue(buffer, index){
+	for(let i=index; i < buffer.length; i++){
+		if(buffer[i]==":"){
+			return i+1
+		}
+	}
+	throw "Error: ':' was not found"
 }
-
-
 
 function getBuffPos(buffer, index){
-  let stack1 = []
-  let stack2 = []
-  let len = buffer.length
-  let start = closestBracketForward(buffer, index)
-  let end = 0
-  let i = start
+	let stack = []
+	let prefchar = ""
+	let len = buffer.length
+	let isString = false
+	let start = closestValue(buffer, index)
+	let end = 0
+	let i = start
 
-  do{
-    let character = buffer[i];
-    if(character == "{"){
-      stack1.push(character)
-    }else if(character == "["){
-      stack2.push(character)
-    }
-    else if(character == "}"){
-      stack1.pop()
-    }else if(character == "]"){
-      stack2.pop()
-    }
-    i++
-    if( i >= len ){ 
-      throw "Error: either you forgot to add '}' or the buffer is corrupted" 
-    }
-  }while( (stack1.length > 0 || stack2.length > 0) && i < len)
-  end=i
-
-  return [start, end]
+	do{
+		let character = buffer[i];
+		if(prefchar == "\\" && isString){
+			//do nothing |:
+		}
+		else if(character == "{"){
+			stack.push(character)
+		}
+		else if(character == "["){
+			stack.push(character)
+		}
+		else if(character == "\"" && !isString){
+			isString = true
+			stack.push(character)
+		}
+		
+		else if(character == "\"" && isString){
+			isString = false
+			stack.pop()
+		}
+		else if(character == "}"){
+			stack.pop()
+		}
+		else if(character == "]"){
+			stack.pop()
+		}
+		i++
+		prefchar = character
+		if( i >= len ){ 
+			throw "Error: either you forgot to add '}' or the buffer is corrupted" 
+		}
+	}while( stack.length > 0 && i < len)
+	end=i
+	console.log(end)
+	return [start, end]
 }
