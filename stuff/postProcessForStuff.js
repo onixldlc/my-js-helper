e4fb9895e8e82bce315bffcb8abfce99da84b088
onixldlc <81515667+onixldlc@users.noreply.github.com>
2022-10-21 16:06:26 +0700

    Create postProcessForStuff.js

---
+++ stuff/postProcessForStuff.js
@@ -0,0 +1,29 @@
+
+function postProcess(arr){
+  return arr.map((value, index)=>{
+    let tempData = createData(value)
+    tempData.index=index;
+    return tempData;
+  })
+}
+
+function createData(data){
+	const dataTemplate = (url, length, creator, title)=>{
+    return{
+      url:url,
+      length:length,
+      creator:creator,
+      title:title
+    }
+  }
+
+  let baseurl=location.origin;
+  let textData = JSON.stringify(data);
+  let id = grabValueFrom(textData, ["webCommand"], {index:2}).url.split("&")[0];
+  
+  let url = baseurl+id;
+  let title = grabValueFrom(textData, "text");
+  let length = grabValueFrom(textData, "lengthSeconds");
+  let creator = grabValueFrom(textData, ["shortBylineText", "text"])
+  return dataTemplate(url, length, creator, title)
+}
