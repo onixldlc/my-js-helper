011c7829ee88a67b0be2fa7c0bf864a71b901dce
onixldlc <81515667+onixldlc@users.noreply.github.com>
2022-10-21 22:39:36 +0800

    Update postProcessForStuff.js

--- stuff/postProcessForStuff.js
+++ stuff/postProcessForStuff.js
@@ -1,30 +1,37 @@
-
-function postProcess(arr){
-  return arr.map((value, index)=>{
-    let tempData = createData(value)
-    tempData.index=index;
-    return tempData;
-  })
+function postProcess(arr, channelName){
+	return arr.map((value, index)=>{
+		let tempData = createData(value, channelName)
+		tempData.index=index;
+		return tempData;
+	})
 }
 
-function createData(data){
+function createData(data, channelName){
 	const dataTemplate = (url, length, creator, title)=>{
-    return{
-      index:0,
-      url:url,
-      length:length,
-      creator:creator,
-      title:title
-    }
-  }
+		return{
+			index:0,
+			url:url,
+			length:length,
+			creator:creator,
+			title:title
+		}
+	}
 
-  let baseurl=location.origin;
-  let textData = JSON.stringify(data);
-  let id = grabValueFrom(textData, ["webCommand"], {index:2}).url.split("&")[0];
-  
-  let url = baseurl+id;
-  let title = grabValueFrom(textData, "text");
-  let length = grabValueFrom(textData, "lengthSeconds");
-  let creator = grabValueFrom(textData, ["shortBylineText", "text"])
-  return dataTemplate(url, length, creator, title)
+	let baseurl=location.origin;
+	let textData = JSON.stringify(data);
+	let id = grabValueFrom(textData, ["webCommand"]).url;
+	
+	let url = baseurl+id;
+	let title = grabValueFrom(textData, "text");
+	let length="";
+	let creator="";
+	if(location.pathname.split("/").at(-1) == "videos"){
+		let tempTime = grabValueFrom(textData, ["TimeStatus", "label"])
+		length = String(convertToSecondSimple(tempTime));
+		creator = channelName
+	}else if(location.pathname.split("/").at(-1) == "playlist"){
+		length = grabValueFrom(textData, "lengthSeconds");
+		creator = grabValueFrom(textData, ["shortBylineText", "text"])
+	}
+	return dataTemplate(url, length, creator, title)
 }
