13ab045ba3ae13c27e2ef50f55827f3ef283631e
onixldlc <81515667+onixldlc@users.noreply.github.com>
2022-10-21 14:42:55 +0700

    Update getTextPos.js

--- getTextPos.js
+++ getTextPos.js
@@ -1,5 +1,5 @@
-function getKeyPos(){
-	return string.split(subString, index).join(subString).length;
+function getKeyPos(buffer, toFind, index){
+	return buffer.split(toFind, index).join(toFind).length;
 }
 
 // https://stackoverflow.com/questions/14480345/how-to-get-the-nth-occurrence-in-a-string
